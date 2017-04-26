edc41d19fda0c0fd173bde21c5f4aa71a8118085
Michael McMullen <michael.mcmullen@tutelagesystems.com>
2017-04-25 21:20:05 -0400

    Added database migration and model
    also removed unwanted migrations such as users and password reset, as they will not be used in this series.

---
+++ app/Models/Email.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Email extends Model
+{
+    protected $table = 'emails';
+
+    protected $fillable = [
+        'email',
+    ];
+}
